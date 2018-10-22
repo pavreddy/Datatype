703dbd4bf797a55a39efe3d8f2d3bd33079b6392
Pavani Gadila <pavanigadila@gmail.com>
2018-10-22 14:25:51 -0400

    first commit while practicing with eclipse to github

---
+++ JavaTrainingSession/src/JavaBasics/DataTypes.java
@@ -0,0 +1,43 @@
+package JavaBasics;
+
+public class DataTypes {
+//Hi this is my first java code
+	/*hi this
+	is my first
+	java code*/
+	
+	//main method is starting point
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive datatypes int char boolean double
+//1i int;
+		int i = 10;
+		i=20;
+		int j = 30;
+		System.out.println(j);
+		System.out.println(i+j);
+		 
+		//2 double
+		double d =12.33;
+		System.out.println(d);
+		double d1 =100;
+		System.out.println(d1);
+		//3 char
+		char c = 'a';
+		char c1= 'A';
+		System.out.println(c1);
+		char c2 = '1';
+		char c3 = '$';
+		System.out.println(c3);
+		//4 boolean
+		boolean b1 = true;
+		boolean b2 = false;
+		//5 string is a class not data type
+		String s = "hello world";
+		System.out.println(s);
+		String s1 = "1000";
+		
+		
+	}
+
+}
